eb2a2a415e101672688cd9f2989cdc292a065a42
Elijah Mooring <45398751+Vehmloewff@users.noreply.github.com>
2021-03-29 20:35:59 -0500

    fix: make dynamic import path absolute

--- lib/execute-task.ts
+++ lib/execute-task.ts
@@ -1,3 +1,5 @@
+import { resolve } from 'https://deno.land/std@0.91.0/path/mod.ts
+
 export type TaskParam = TaskArgument | TaskOptions
 export type TaskArgument = string | number
 export interface TaskOptions {
@@ -5,7 +7,7 @@ export interface TaskOptions {
 }
 
 export async function executeTask(tasksFile: string, task: string, params: TaskParam[]) {
-	const module = await import(`file://${tasksFile}`)
+	const module = await import(`file://${resolve(tasksFile)}`)
 
 	if (!module[task]) throw new Error(`The checks failed to fully check!`)
 	await module[task](...params)
